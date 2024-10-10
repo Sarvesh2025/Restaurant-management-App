7fbb9351cade895ab76a9ba74e23e55c65aeeeb6
Sarvesh2025 <utsavsarveshpandey@gmail.com>
2024-10-10 15:00:42 +0530

    Database connection is updated

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -5,30 +5,35 @@ import (
 	"fmt"
 	"log"
 	"time"
-
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-drivier/mongo/options "
 )
 
-func DBinstance() *mongo.Client{
-	MongoDb:="mongodb://localhost:27017"
+func DBinstance() *mongo.Client {
+	MongoDb := "mongodb://localhost:27017"
 	fmt.Print(MongoDb)
-
-	client,err:=mongo.NewClient(options.Client().ApplyURI(MongoDb))
-	if err!=nil{
-		log.fatal(err)
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		log.Fatal(err)
 	}
-	ctx,cancel:=context.WithTimeout(context.Background(),10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
 	defer cancel()
 
-	err=client.Connect(ctx)
+	err = client.Connect(ctx)
 
-	if err!=nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Connected to mongodb")
 	return client
 
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.database("restaurant").Collection(collectionName)
+
+	return collection
 }
